2dd9d93fe11fb9b08f02048eef128cf0728997ae
lunaseakyo <eric.fan.zy@googlemail.com>
2017-02-16 16:23:25 +0800

    '20170216'

---
+++ js/sidebar.js
@@ -0,0 +1,6 @@
+/**
+     * Created by fanzy05 on 2017/2/15.
+     */
+    <script type="text/javascript">
+document.getElementById("sidebar").innerHTML = "<a href='../sidebar.html'></a>"
+    </script>
